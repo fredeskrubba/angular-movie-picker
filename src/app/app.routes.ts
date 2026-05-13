71a7356ce33e01c0630805dc2f372ba135632dbc
Fredeskrubba <frederikskrubbeltrang@proton.me>
2026-05-13 14:50:33 +0200

    added routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path: "",
+    pathMatch: "full",
+    loadComponent: () => {
+        return import("./home/home").then(
+            m => m.Home
+        )
+    }},
+    {
+        path: "watchlist",
+        pathMatch: "full",
+        loadComponent: () => {
+            return import("./watchlist/watchlist").then(
+                m => m.Watchlist
+            )
+        }
+    }
+];
--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Icon} from '../../shared/icon/icon';
+import { RouterLink, RouterLinkActive } from "@angular/router";
 
 @Component({
   selector: 'app-navbar',
-  imports: [Icon],
+  imports: [Icon, RouterLink, RouterLinkActive],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
