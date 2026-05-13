49f66a201c11d23247e0de05fc3767143c638b01
Fredeskrubba <frederikskrubbeltrang@proton.me>
2026-05-13 14:11:28 +0200

    enhance navbar with responsive styles and icon support; add randomize button component

---
+++ src/app/components/randomize-button/randomize-button.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-randomize-button',
+  imports: [],
+  templateUrl: './randomize-button.html',
+  styleUrl: './randomize-button.css',
+})
+export class RandomizeButton {}
--- src/app/shared/icon/icon.ts
+++ src/app/shared/icon/icon.ts
@@ -5,8 +5,9 @@ import { CommonModule } from '@angular/common';
   selector: 'app-icon',
   imports: [CommonModule],
   templateUrl: './icon.html',
+  styleUrl: './icon.css',
 })
+
 export class Icon {
   @Input() name!: string;
-  @Input() size: number = 24;
 }
