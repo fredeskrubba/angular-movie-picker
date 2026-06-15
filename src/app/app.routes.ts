7f2a2d8882f845d9c361c3ac9cb6f8b5adad78f4
fredeskrubba <frederikskrubbeltrang@proton.me>
2026-06-15 11:33:40 +0200

    add login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,53 +1,71 @@
 import { Routes } from '@angular/router';
 
-
 export const routes: Routes = [
-    {
+  {
     path: '',
     pathMatch: 'full',
     redirectTo: 'login'
-    },
-    {
-    path: "login",
-    pathMatch: "full",
-    loadComponent: () => {
-        return import("./login/login").then(
-            m => m.Login
-        )
-    }},
-    {
-    path: "browse",
-    pathMatch: "full",
-    loadComponent: () => {
-        return import("./home/home").then(
-            m => m.Home
-        )
-    }},
-    {
-        path: "watchlist",
-        pathMatch: "full",
-        loadComponent: () => {
-            return import("./watchlist/watchlist").then(
-                m => m.Watchlist
-            )
-        }
-    },
-    {
-        path: "about",
-        pathMatch: "full",
-        loadComponent: () => {
-            return import("./about/about").then(
-                m => m.About
-            )
-        }
-    },
-    {
-        path: 'movies/:id',
-        loadComponent: () => {
-            return import("./movie-details-mobile/movie-details-mobile").then(
-                m => m.MovieDetailsMobile
-            )
-        }
+  },
 
-    }
+ 
+  {
+    path: '',
+    loadComponent: () =>
+      import('./layouts/auth-layout/auth-layout').then(
+        m => m.AuthLayout
+      ),
+
+    children: [
+      {
+        path: 'login',
+        loadComponent: () =>
+          import('./login/login').then(
+            m => m.Login
+          )
+      }
+    ]
+  },
+
+
+  {
+    path: '',
+    loadComponent: () =>
+      import('./layouts/main-layout/main-layout').then(
+        m => m.MainLayout
+      ),
+
+    children: [
+      {
+        path: 'browse',
+        loadComponent: () =>
+          import('./home/home').then(
+            m => m.Home
+          )
+      },
+
+      {
+        path: 'watchlist',
+        loadComponent: () =>
+          import('./watchlist/watchlist').then(
+            m => m.Watchlist
+          )
+      },
+
+      {
+        path: 'about',
+        loadComponent: () =>
+          import('./about/about').then(
+            m => m.About
+          )
+      },
+
+      {
+        path: 'movies/:id',
+        loadComponent: () =>
+          import('./movie-details-mobile/movie-details-mobile').then(
+            m => m.MovieDetailsMobile
+          )
+      }
+    ]
+  }
 ];
