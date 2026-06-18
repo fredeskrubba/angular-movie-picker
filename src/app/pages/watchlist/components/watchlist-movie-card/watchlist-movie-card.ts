d1d0fe8866a0f82453748dbc8742a55a9eb8ed70
fredeskrubba <frederikskrubbeltrang@proton.me>
2026-06-18 12:19:09 +0200

    watchlist actions

--- src/app/pages/watchlist/components/watchlist-movie-card/watchlist-movie-card.ts
+++ src/app/pages/watchlist/components/watchlist-movie-card/watchlist-movie-card.ts
@@ -1,6 +1,8 @@
-import { Component, input } from '@angular/core';
+import { Component, input, signal, inject } from '@angular/core';
 import { Movie } from '../../../../models/movie';
 import { Icon } from '../../../../global-components/icon/icon';
+import { StorageService } from '../../../../services/storage';
+
 
 @Component({
   selector: 'app-watchlist-movie-card',
@@ -10,5 +12,14 @@ import { Icon } from '../../../../global-components/icon/icon';
 })
 
 export class WatchlistMovieCard {
+    storageService = inject(StorageService)
     watchlistMovie = input.required<Movie>();
+    isOpen = signal(false);
+
+    removeFromWatchlist(e: MouseEvent) {
+      e.stopPropagation();
+      this.storageService.removeItem(this.watchlistMovie().title);
+      console.log(`Removing ${this.watchlistMovie().title} from watchlist`);
+      console.log(this.storageService.getItem("Watchlist"));
+    }
 }
--- src/app/pages/watchlist/watchlist.ts
+++ src/app/pages/watchlist/watchlist.ts
@@ -3,10 +3,11 @@ import {StorageService} from '../../services/storage'
 import {OnInit} from '@angular/core';
 import { WatchlistMovieCard } from './components/watchlist-movie-card/watchlist-movie-card';
 import { WatchlistFilter } from './watchlist.enum';
+import { Icon } from '../../global-components/icon/icon';
 
 @Component({
   selector: 'app-watchlist',
-  imports: [WatchlistMovieCard],
+  imports: [WatchlistMovieCard, Icon],
   templateUrl: './watchlist.html',
   styleUrl: './watchlist.css',
 })
@@ -16,6 +17,7 @@ export class Watchlist implements OnInit {
   storageService = inject(StorageService)
   watchlistFilter = WatchlistFilter;
 
+  searchQuery = signal("");
   currentFilter = signal(WatchlistFilter.All);
 
 
@@ -26,6 +28,11 @@ export class Watchlist implements OnInit {
   changeFilter(filter: WatchlistFilter) {
     this.currentFilter.set(filter);
     console.log(this.currentFilter());
+  }
+
+  onSearchUpdated(query: string) {
+    console.log("Search query updated:", query);
+    // Implement search logic here, e.g., filter the watchlist based on the query
   }
 
   ngOnInit() {
