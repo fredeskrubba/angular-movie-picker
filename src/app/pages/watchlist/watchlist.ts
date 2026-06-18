89beea0ff130c8fc6bc1c4e2dfad6e5e74709bc1
fredeskrubba <frederikskrubbeltrang@proton.me>
2026-06-18 13:05:47 +0200

    add search logic in watchlist

--- src/app/pages/watchlist/watchlist.ts
+++ src/app/pages/watchlist/watchlist.ts
@@ -1,9 +1,10 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, signal, computed } from '@angular/core';
 import {StorageService} from '../../services/storage'
 import {OnInit} from '@angular/core';
 import { WatchlistMovieCard } from './components/watchlist-movie-card/watchlist-movie-card';
 import { WatchlistFilter } from './watchlist.enum';
 import { Icon } from '../../global-components/icon/icon';
+import { Movie } from '../../models/movie';
 
 @Component({
   selector: 'app-watchlist',
@@ -20,19 +21,27 @@ export class Watchlist implements OnInit {
   searchQuery = signal("");
   currentFilter = signal(WatchlistFilter.All);
 
+  items = computed(() => {
+    const sq = this.searchQuery().toLowerCase();
 
-  getWatchlist() {
-    return this.storageService.getItem("Watchlist");
+    return (this.getWatchlist() ?? []).filter(movie =>
+      movie.title.toLowerCase().includes(sq)
+    );
+  });
+
+  getWatchlist(): Movie[] | null {
+    const data = this.storageService.getItem("Watchlist");
+    return data ? data as Movie[] : null;
   }
 
   changeFilter(filter: WatchlistFilter) {
     this.currentFilter.set(filter);
-    console.log(this.currentFilter());
   }
 
   onSearchUpdated(query: string) {
-    console.log("Search query updated:", query);
-    // Implement search logic here, e.g., filter the watchlist based on the query
+   
+    this.searchQuery.set(query);
+    
   }
 
   ngOnInit() {
